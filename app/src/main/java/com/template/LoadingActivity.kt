9d0307736435dca589ba1cede38cb8e36d632e1c
aleksandr.genrikhs <aleksandr.genrikhs@gmail.com>
2023-06-15 14:32:29 +1100

    add Firebase add google-services.json create WebViewModel

--- app/src/main/java/com/template/LoadingActivity.kt
+++ app/src/main/java/com/template/LoadingActivity.kt
@@ -1,15 +1,52 @@
 package com.template
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.template.databinding.ActivityLoadingBinding
 
 class LoadingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoadingBinding
+
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //get database link
+        database = Firebase.database.reference
+
+        //enable disk persistence
+      //  Firebase.database.setPersistenceEnabled(true)
+
+        //connection status
+        val connectedRef = Firebase.database.getReference(".info/connected")
+        connectedRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val connected = snapshot.getValue(Boolean::class.java) ?: false
+                if (connected) {
+                    Log.d("Connected", "connected")
+                } else {
+                    Toast.makeText(this@LoadingActivity, "not connected", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this@LoadingActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("Connected", "Listener was cancelled")
+            }
+        })
+
     }
 }
